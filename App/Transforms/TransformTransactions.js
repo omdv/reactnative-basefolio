562c146d14dc0482ee936ac498042143443c9c79
Oleg Medvedev <omdv@fastmail.com>
2017-10-19 21:31:10 -0500

    Changed to "current_prices" scheme

--- App/Transforms/TransformTransactions.js
+++ App/Transforms/TransformTransactions.js
@@ -1,17 +1,24 @@
+// export default (trans) => {
+//     // flatten and convert to numeric
+//     trans = trans.map(x => x.data)
+
+//     trans.map(t => {
+//     	t.map(e => {
+//     		e.amount.amount = Number(e.amount.amount)
+//     		e.native_amount.amount = Number(e.native_amount.amount)
+//     	})
+//     })
+//     return trans
+// }
+
+
 export default (trans) => {
     // flatten and convert to numeric
-    trans = trans.map(x => x.data)
-    // trans = trans.reduce((x,y) => x.concat(y))
-    // trans.map(e => {
-    //   e.amount.amount = Number(e.amount.amount)
-    //   e.native_amount.amount = Number(e.native_amount.amount)
-    // })
+    trans = trans.data
 
-    trans.map(t => {
-    	t.map(e => {
-    		e.amount.amount = Number(e.amount.amount)
-    		e.native_amount.amount = Number(e.native_amount.amount)
-    	})
+    trans.map(e => {
+        e.amount.amount = Number(e.amount.amount)
+        e.native_amount.amount = Number(e.native_amount.amount)
     })
     return trans
 }
